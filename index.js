4df95d98350398e2cd5807c4777e0b0bccf12a18
riazakhanda017ksn <riazkahanda@gmail.com>
2022-02-12 11:22:21 -0800

    rest api basic example

--- index.js
+++ index.js
@@ -1,39 +1,83 @@
+const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+const port = 5000;
 mongoose
-  .connect("mongodb://localhost:27017/Sample", {
+  .connect("mongodb://127.0.0.1:27017/Sample", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log("connected to the mongodb successfully");
+    console.log("database connected out");
   })
   .catch((err) => {
     console.log(err);
   });
 
-const student = new mongoose.Schema({
+const ProductSchema = new mongoose.Schema({
   name: String,
-  workOut: Boolean,
-  height: Number,
+  description: String,
+  price: Number,
 });
 
-const Student = new mongoose.model("Student", student); ///model is like = collections
+const products = new mongoose.model("Product", ProductSchema);
 
-const adder = async () => {
-  //   const get = await Student.find({ name: { $eq: "riaz" } });   eq= equal
-  //   const get = await Student.find({ name: { $gt: "riaz" } }); gt = greater
-  //   const get = await Student.find({ name: { $gte: "riaz" } }); gte = greater then or equal
-  //   const get = await Student.find({ name: { $in: ["riaz", "Riaz"] } }); in = [in array has thats all  value]
-  //   const get = await Student.find({ name: { $nin: ["riaz", "Riaz"] } });[]
-  const get = await Student.find({ name: { $in: ["riaz", "Riaz"] } });
+app.post("/api/v1/addProduct/new", async (req, res) => {
+  const product = await products.create(req.body);
+  res.status(200).json({
+    success: true,
+    product,
+  });
+});
 
-  //   const get = await Student.find();
+app.get("/api/v1/getAllProduct", async (req, res) => {
+  const getProducts = await products.find();
+  res.status(200).json({
+    success: true,
+    getProducts,
+  });
+});
+
+app.put("/api/v1/product/:id", async (req, res) => {
+  let product = await products.findById(req.params.id);
+  if (!product) {
+    res.status(501).json({
+      success: false,
+      message: "product is not found !",
+    });
+  }
+  product = await products.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    useFindAndModify: false,
+    runValidators: true,
+  });
+  res.status(200).json({
+    success: true,
+    product,
+  });
+});
+
+app.delete("/api/v1/product/:id", async (req, res) => {
+  let product = await products.findById(req.params.id);
+  if (!product) {
+    res.status(501).json({
+      success: false,
+      message: "product is not found !",
+    });
+  }
+  product.remove();
+  res.status(200).json({
+    success: true,
+    message: "product deleted",
+  });
+});
+app.get("/", async (req, res) => {
+  const get = await res.send("hello world");
   console.log(get);
-  //   await Student.create({
-  //     name: "Riaz",
-  //     workOut: false,
-  //     height: 5.2,
-  //   });
-};
-
-adder();
+});
+app.listen(port, () => {
+  console.log(`server is running on  ${port}`);
+});
